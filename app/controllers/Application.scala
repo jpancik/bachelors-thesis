eda3a4defa40774123d84bf0c526fe1f12a05cb7
Juraj Pancik <jpancik@gmail.com>
2017-03-06 15:47:55 +0100

    deployed on digital ocean

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -55,8 +55,8 @@ class Application @Inject() (val phrasesManager: PhrasesManager) extends Control
 
     def test = Action {
         val filePath = "data/test-data-wikipedia.500.json"
-        val json = Json.parse(Source.fromFile("public/" + filePath).mkString);
-        Ok(views.html.test(filePath, json.toString()))
+        //val json = Json.parse(Source.fromFile("public/" + filePath).mkString);
+        Ok(views.html.test(filePath))
     }
 
     def getSuggestions(sentence: String) = Action.async {
